21dff758b2c1d2eddbc81a3620ae6ad16de567d7
nameissrikanth2303 <chsrikanth2303@gmail.com>
2021-06-13 23:29:35 +0530

    publish

--- src/Nav.js
+++ src/Nav.js
@@ -2,21 +2,20 @@ import React, { useState, useEffect } from "react";
 import "./Nav.css";
 
 function Nav() {
-   const [show, handleShow] = useState(false);
-    useEffect (() => {
-        window.addEventListener("scroll", () => {
-            if (window.scrollY > 100) {
-                handleShow(true);
-            }
-        else handleShow(false);
-        });
-        return ()=> {
-            window.removeEventListener("scroll");
-        };
-    },[]);
+  const [show, handleShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
   return (
     //
-    <div className={`"nav" ${show && "nav__black"}`}>
+    <div className={`nav  ${show && "nav__black"}`}>
       <img
         className="logo-netflix"
         src="https://1000logos.net/wp-content/uploads/2017/05/Netflix-Logo.png"
